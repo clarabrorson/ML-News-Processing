975bcd4a2d8a65235346153b3c5a4eb337c5eb93
clarabrorson <clarabrorson@hotmail.com>
2025-02-20 10:12:24 +0100

    changed docstring

--- web/app.py
+++ web/app.py
@@ -4,24 +4,22 @@ from ui import display_news, display_filter_search
 import os
 from PIL import Image
 
-"""
-This is the main file for the Streamlit web application, it includes the main function that is run when the application is started.
-To start the application run the following command in the terminal:
-streamlit run app.py
+# This is the main file for the Streamlit web application, it includes the main function that is run when the application is started.
+# To start the application run the following command in the terminal:
+# streamlit run app.py
+#
+# The file also includes the following functions:
+# - show_image: Displays an image in the center of the page.
+# - set_background_and_text_color: Sets the background color to white and the text color to black.
+#
+# HTML and CSS code is used to style the page and the elements on the page.
+# To center the image on the page, the image is placed in the middle column of a three column layout.
+#
+# An if statement is used to check if the data has already been fetched from the database and stored in the session_state.
+# If the data has not been fetched, the data is fetched and stored in the session_state.
+#
+# The main function also calls the display_filter_search and display_news functions from the ui.py file to display the filter and search bar and the news articles on the page.
 
-The file also includes the following functions:
-- show_image: Displays an image in the center of the page.
-- set_background_and_text_color: Sets the background color to white and the text color to black.
-
-HTML and CSS code is used to style the page and the elements on the page.
-To center the image on the page, the image is placed in the middle column of a three column layout.
-
-An if statement is used to check if the data has already been fetched from the database and stored in the session_state.
-If the data has not been fetched, the data is fetched and stored in the session_state.
-
-The main function also calls the display_filter_search and display_news functions from the ui.py file to display the filter and search bar and the news articles on the page.
-
-"""
 
 def show_image():
     image = Image.open("news.jpg")
